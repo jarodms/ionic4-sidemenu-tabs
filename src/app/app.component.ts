397fcb6f0f5f457d20a9d9628adc796617ce8965
Jarod Smith <jarodms@gmail.com>
2019-05-14 21:55:27 -0500

    initial working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { TabsPage } from './tabs/tabs.page';
+import { Tab1Page } from './tab1/tab1.page';
+import { Tab2Page } from './tab2/tab2.page';
+import { Tab3Page } from './tab3/tab3.page';
+
+export interface PageInterface {
+  title: string;
+  name: string;
+  component: any;
+  icon: string;
+  logsOut?: boolean;
+  index?: number;
+  tabName?: string;
+  tabComponent?: any;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  
+  rootPage:any = TabsPage;
+  // pages: Array<{title: string, component: any}>;
+/*
+  appPages: PageInterface[] = [
+    { title: 'Tab1', name: 'TabsPage', component: TabsPage, tabComponent: Tab1Page, index: 0, icon: 'calendar' },
+    { title: 'Tab2', name: 'TabsPage', component: TabsPage, tabComponent: Tab2Page, index: 1, icon: 'contacts' },    
+    { title: 'Tab3', name: 'TabsPage', component: TabsPage, tabComponent: Tab3Page, index: 2, icon: 'information-circle' },
+    //{ title: 'Map', name: 'TabsPage', component: TabsPage, tabComponent: MapPage, index: 3, icon: 'map' }
+  ];
+*/
+public appPages = [
+  {
+    title: 'Home',
+    url: '/tabs/tab1',
+    icon: 'home'
+  },
+  {
+    title: 'List',
+    url: '/tabs/tab2',
+    icon: 'list'
+  },
+  {
+    title: 'Tab 3',
+    url: '/tabs/tab3',
+    icon: 'contact'
+  }
+];
+
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -19,8 +64,33 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  openPage(page: PageInterface) {
+    let params = {};
+
+    // the nav component was found using @ViewChild(Nav)
+    // setRoot on the nav to remove previous pages and only have this page
+    // we wouldn't want the back button to show in this scenario
+    if (page.index) {
+      params = { tabIndex: page.index };
+    }
+
+    // If we are already on tabs just change the selected tab
+    // don't setRoot again, this maintains the history stack of the
+    // tabs even if changing them from the menu
+    /*if (this.nav.getActiveChildNavs().length && page.index != undefined) {
+      this.nav.getActiveChildNavs()[0].select(page.index);
+    } else {
+      // Set the root of the nav with params if it's a tab index
+      this.nav.setRoot(page.name, params).catch((err: any) => {
+        console.log(`Didn't set nav root: ${err}`);
+      });
+    } */   
   }
 }
